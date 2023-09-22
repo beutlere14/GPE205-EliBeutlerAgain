2d1e340f3730a41440aa36c88a47d4aff8998f07
Eli Beutler <elibeutler@gmail.com>
2023-09-22 15:16:54 -0700

    Added Controlls, No movement Yet. Just signals to show the inputs work.

---
+++ Assets/Script/Controllers/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Controller
+{
+    public KeyCode moveForwardKey;
+    public KeyCode moveBackwardKey;
+    public KeyCode rotateClockwiseKey;
+    public KeyCode rotateCounterClockwiseKey;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+
+        ProcessInputs();
+    }
+
+    public void ProcessInputs()
+    {
+        if (Input.GetKey(moveForwardKey))
+        {
+            pawn.MoveForward();
+        }
+
+        if(Input.GetKey(moveBackwardKey))
+        {
+            pawn.MoveBackward();
+        }
+
+        if (Input.GetKey(rotateClockwiseKey))
+        {
+            pawn.RotateClockwise();
+        }
+
+        if( Input.GetKey(rotateCounterClockwiseKey)) 
+        {
+            pawn.RotateCounterClockwise();
+        }
+    }
+}
